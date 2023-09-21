502ee7d6b3106ee96df1cc00edd5047a7c8a924c
Vedant336 <vedantreigns11@gmail.com>
2023-09-22 02:14:20 +0530

    Variables & Constant demo w Table (Console.table)

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14533
+let accountEmail = "vedant@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nadiad"
+
+// accountId = 2 //won't be changed because it's constant
+
+accountEmail = "v2@gmail.com"
+accountPassword = "67890"
+accountCity = "Vadodara"
+
+
+//display in tabular structure
+console.table([accountId, accountEmail, accountPassword, accountCity])
